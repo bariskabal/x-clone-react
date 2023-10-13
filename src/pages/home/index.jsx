d29be3d1f70ffdad3162dcb732190e31c86b64a2
bariskabal12 <bariskabal12@gmail.com>
2023-10-13 13:25:54 +0300

    tab component

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,29 @@
+import StickyHeader from "~/components/sticky-header";
+import Tab from "~/components/tab";
+
 export default function Home() {
+
     return(
-        <div>Home Component</div>
+        <>
+            <Tab activeTab="for-you">
+                <StickyHeader title="Anasayfa">
+                    <Tab.Items>
+                        <Tab.Item id="for-you">
+                            Sana özel
+                        </Tab.Item>
+                        <Tab.Item id="followings">
+                            Takip edilenler
+                        </Tab.Item>
+                    </Tab.Items>
+                </StickyHeader>
+                <Tab.Content id="for-you">
+                    1. content
+                </Tab.Content>
+                <Tab.Content id="followings">
+                    2. content
+                </Tab.Content>
+            </Tab>
+            Home Component
+        </>
     )
 }
